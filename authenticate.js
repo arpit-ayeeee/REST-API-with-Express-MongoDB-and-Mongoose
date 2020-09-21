2f758eb03d0407392e0c6906ab06dd23369245c9
Arpit Jaiswal <arpitjaiswal282000@gmail.com>
2020-09-21 23:22:07 +0530

    Updated readme

--- authenticate.js
+++ authenticate.js
@@ -14,4 +14,4 @@ passport.deserializeUser(User.deserializeUser());
 //The verify function will be called with the Username and Password, that the passport will extract from the incoming request
 //Since we used the passport-local-mongoose plugin, it adds a method to User schema called authenticate 
 //The autheticate method will itself act as an authetication function for the LocalStrategy
-//Serialize and deserialize methods are provided by the mongoose plugin in User schema, which take cares for the support for sessions in passport
+//Serialize and deserialize methods are provided by the mongoose plugin in User schema, which take cares for the support for sessions in passport.
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
-This project is developed to perform REST API.
+This project is developed to perform REST API, and backend services.
 I've used an Express generator, and router for the diffrent routes.
 I've used MongoDB as database and Mongoose for schemas.
 Using all three, GET, PUT, POST, and DELETE operations can be performed once the server is up.
+I've added basic cookie-based authentication initially, then I upgraded it with session-based auth, and currently it's passport-based auth.
